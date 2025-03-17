c08954b827c52bfa3ad483c8c344bd57dd9f6300
andabi <abigaell.andrey@hes-so.ch>
2025-03-17 20:39:58 +0100

    moddify files

--- src/s04/BubbleOnLists.java
+++ src/s04/BubbleOnLists.java
@@ -1,24 +1,33 @@
 package s04;
 
 import s03.List;
-import s03.ListItr; 
+import s03.ListItr;
 
 public class BubbleOnLists {
 
-  static void bubbleSortList(List l) {
-    if (l.isEmpty()) return;
-    ListItr li = new ListItr(l);
-    boolean goOn = true;
-    while(goOn) {
-      // TODO 
-    }
-  }
+    static void bubbleSortList(List l) {
+        if (l.isEmpty()) return;
+        boolean goOn = true;
+        while (goOn) {
+            goOn = false;
+            ListItr li = new ListItr(l); // Réinitialisation de l'itérateur à chaque passe
 
-  //Swaps between left and right element if needed
-  //Returns true if swap occurred
-  static boolean bubbleSwapped(ListItr li) {
-    if (li.isFirst() || li.isLast()) return false;
-    // TODO 
-    return true;
-  }
+            while (!li.isLast()) { // Parcours jusqu'à l'avant-dernier élément
+                if (bubbleSwapped(li)) {
+                    goOn = true; // Un échange a eu lieu, il faut refaire une passe
+                }
+                li.goToNext(); // Passer à l'élément suivant
+            }
+        }
+    }
+
+    static boolean bubbleSwapped(ListItr li) {
+        if (li.isFirst() || li.isLast()) return false;
+
+        //todo
+        return false;
+    }
+
 }
+
+
