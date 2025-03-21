65fa94e849d298085cbb268efac3a726d08f109f
elvin.kuci <elvin.kuci@hes-so.ch>
2025-03-21 11:28:28 +0100

    Do Ex1

--- src/s06/Ex1SetDemo.java
+++ src/s06/Ex1SetDemo.java
@@ -3,11 +3,17 @@ package s06;
 
 public class Ex1SetDemo {
 
+  /// Returns a subset of s where values are lower than x
+  /// @param s The set we'll get a subset of
+  /// @param x The value the subset's values should be lower than
+  /// @return The subset of s where the value are lower than x
   public static SetOfShorts z(SetOfShorts s, int x) {
     SetOfShorts res = new SetOfShorts();
+    // Iterates through the superset
     SetOfShortsItr itr = s.iterator();
     while (itr.hasMoreElements()) {
       short i = itr.nextElement();
+      // Only add a value in the subset if it's lower than x
       if (i<x) res.add(i);
     }
     return res;
