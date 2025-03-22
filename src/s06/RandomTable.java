f332d96309c870a54270d95facd3f10617916533
elvin.kuci <elvin.kuci@hes-so.ch>
2025-03-22 21:01:25 +0100

    Add S06 Ex.3

--- src/s06/RandomTable.java
+++ src/s06/RandomTable.java
@@ -10,14 +10,17 @@ public class RandomTable {
   // Returns: a observedOutput array with randomly chosen distinct elements in [0..n-1]
   public static short[] randomTable(short m, short n) {
     short[] result = null;
-
-    // TODO 
-    // PSEUDOCODE : 
-    //     create an empty set
-    //     while the set has less than m elements
-    //       add to the set a randomly chosen element  
-    //     with an iterator, copy the entire set into an array
-    //     sort the array
+    // Fill up the set
+    SetOfShorts set = new SetOfShorts();
+    while (set.size() < m) {
+      set.add((short)rnd.nextInt(n)); // Bound of `n` means from 0 (inclusive) to n (exclusive)
+    }
+    // Copy to result
+    result = new short[set.size()];
+    SetOfShortsItr it = set.iterator();
+    for (int i = 0; it.hasMoreElements(); i++) {
+      result[i] = it.nextElement();
+    }
     Arrays.sort(result);
     return result;
   }
