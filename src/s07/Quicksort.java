b247526ca4bb4235a6eb718b1d6ff4d866770fa0
elvin.kuci <elvin.kuci@hes-so.ch>
2025-04-01 21:12:13 +0200

    Add S07 Ex2+

--- src/s07/Quicksort.java
+++ src/s07/Quicksort.java
@@ -15,8 +15,35 @@ public class Quicksort {
   // chooses a pivot, and partitions the sub-array
   // Returns the final position of the pivot
   private static int partition(int[] t, int left, int right) {
-    return 0; // TODO
+    // choisir le premier élément comme pivot
+    int p = t[left];
+    int k = left;
+    // pour chaque case suivante i de left+1 à right:
+    for (int i = left+1; i <= right; i++) {
+      // si l'élément courant doit aller dans la partie grisée
+      if (t[i] <= p) {
+        // agrandir la partie grisée, en échangeant
+        k++;
+        int tmp = t[k];
+        t[k] = t[i];
+        t[i] = tmp;
+      }
+    }
+
+    // mettre le pivot à la fin de la partie grisée
+    int tmp = t[left];
+    t[left] = t[k];
+    t[k] = tmp;
+
+    // retourner la position de l'extrémité droite de la
+    return k;
   }
+
+  /*
+   * Pk <= pivot et pas < pivot ?
+   * - Stabilité: si < pivot, alors les éléments égaux au pivot seront swaped
+   * - WorstCaseSenario: si < pivot, n'itère pas le k si le tab like [1,1,1,1,1,1,1], donc O(n^2) au lieu de O(n)
+   */
 
   private static void quickSort(int[] t, int left, int right) {
     if (left >= right)
