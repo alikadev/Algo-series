cb85a63eb7eacf0f420868eb3ff544e3109b9b2d
elvin.kuci <elvin.kuci@hes-so.ch>
2025-05-05 13:24:05 +0200

    Do S10.3a

--- src/s10/QueueChained.java
+++ src/s10/QueueChained.java
@@ -1,23 +1,42 @@
 package s10;
 
 public class QueueChained<E> {
+  private static class QueueNode<E> {
+    final E elt;
+    QueueNode<E> next = null;
+    QueueNode(E elt) { this.elt = elt; }
+  }
+  // ======================================================================
+  private QueueNode<E> front;
+  private QueueNode<E> back;
   public QueueChained() {}
 
   public void enqueue (E elt) {
-    return; // TODO
+    QueueNode<E> node = new QueueNode<>(elt);
+    // Update front / last back references
+    if (back == null) front = node;
+    else back.next = node;
+    // Push back
+    back = node;
   }
 
   public boolean isEmpty() {
-    return false; // TODO
+    return back==null;
   }
 
   // PRE : !isEmpty()
   public E consult()     {
-    return null; // TODO
+    return front.elt;
   }
 
   // PRE : !isEmpty()
   public E dequeue() {
-    return null; // TODO
+    E e = front.elt;
+    if (front == back) {
+      back = null; front = null;
+    } else {
+      front = front.next;
+    }
+    return e;
   }
 }
