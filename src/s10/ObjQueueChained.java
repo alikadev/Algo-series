bc7ad2a3102a24bfad1480afd07192d4db38bcf3
elvin.kuci <elvin.kuci@hes-so.ch>
2025-05-05 13:51:28 +0200

    Do S10.3c

--- src/s10/ObjQueueChained.java
+++ src/s10/ObjQueueChained.java
@@ -2,25 +2,40 @@ package s10;
 
 // This is a version using explicit Object (as if Java was not offering generic types)
 public class ObjQueueChained {
+  private static class QueueNode {
+    final Object elt;
+    QueueNode next = null;
+    QueueNode(Object elt) { this.elt = elt; }
+  }
+  // ======================================================================
+  private QueueNode front;
+  private QueueNode back;
   public ObjQueueChained() {}
 
   public void enqueue (Object elt) {
-    // TODO
+    QueueNode node = new QueueNode(elt);
+    // Update front / last back references
+    if (back == null) front = node;
+    else back.next = node;
+    // Push back
+    back = node;
   }
 
   public boolean isEmpty() {
-    return false; // TODO
+    return back==null;
   }
 
   public Object consult() {
-    return null; // TODO
-  }
-
-  public int size() {
-    return 0; // TODO
+    return front.elt;
   }
 
   public Object dequeue() {
-    return null; // TODO
+    Object e = front.elt;
+    if (front == back) {
+      back = null; front = null;
+    } else {
+      front = front.next;
+    }
+    return e;
   }
 }
