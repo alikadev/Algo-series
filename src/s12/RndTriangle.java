89217570a9c1da5be261cdd7503f670fee383658
elvin.kuci <elvin.kuci@hes-so.ch>
2025-05-19 09:58:08 +0200

    Add Ex1

--- src/s12/RndTriangle.java
+++ src/s12/RndTriangle.java
@@ -9,6 +9,16 @@ public class RndTriangle {
   }
   //============================================================
   public static double rndTriangleAvgArea(Random r, int nbOfExperiments) {
-    return 0.0; // TODO
+    double sum = 0.0;
+    for (int i = 0; i < nbOfExperiments; i++) {
+        final double x1 = r.nextDouble();
+        final double x2 = r.nextDouble();
+        final double x3 = r.nextDouble();
+        final double y1 = r.nextDouble();
+        final double y2 = r.nextDouble();
+        final double y3 = r.nextDouble();
+        sum += 0.5 * Math.abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2));
+    }
+    return sum / nbOfExperiments;
   }
 }
