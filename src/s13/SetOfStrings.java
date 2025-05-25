85cffc539fd2cd0be027105b7498180fdabdff68
elvin.kuci <elvin.kuci@hes-so.ch>
2025-05-25 20:18:21 +0200

    Add S13.E3

--- src/s13/SetOfStrings.java
+++ src/s13/SetOfStrings.java
@@ -38,17 +38,56 @@ public class SetOfStrings {
   // absent, the index of an appropriate free cell 
   // where e can be stored
   int targetIndex(String e) { 
-    return 0; // TODO 
+    /* Pseudo-code:
+    ...
+    */
+    int hash = hashString(e);
+    int pos = hash;
+    int count = total[pos];
+
+    while (count > 0) {
+      pos = busy.nextSetBit(pos);
+      if (pos < 0) pos = busy.nextSetBit(0);
+      if (hashString(elt[pos]) == hash) {
+        if (e.equals(elt[pos])) return pos;
+        count--;
+      }
+      pos = (pos + 1) % capacity();
+    }
+
+    pos = busy.nextClearBit(hash);
+    if (pos >= capacity()) return busy.nextClearBit(0);
+    return pos;
   }
 
   public void add(String e) {
     if (currentSize *2 >= capacity())
       doubleTable();
-    // TODO 
+    int idx = targetIndex(e);
+    if (busy.get(idx)) return;
+    busy.set(idx);
+    elt[idx] = e;
+    total[hashString(e)]++;
+    currentSize++;
   } 
 
   private void doubleTable() {
-    // TODO 
+    // Backup needed attrs to reconstruct the entire table
+    String[] eltBkp = elt;
+    BitSet busyBkp = busy;
+
+    // Reinit attrs and double their length
+    total = new int[2 * total.length];
+    elt = new String[2 * eltBkp.length];
+    busy = new BitSet(2 * busy.length());
+    currentSize = 0;
+
+    // Restore the elements back in the new hash-table
+    int idx = busyBkp.nextSetBit(0);
+    while (idx >= 0) {
+      add(eltBkp[idx]);
+      idx = busyBkp.nextSetBit(idx + 1);
+    }
   }
 
   public void remove(String e) {
@@ -66,15 +105,35 @@ public class SetOfStrings {
   }
   
   public SetOfStringsItr iterator() {
-    return null;     // TODO 
+    return new SetOfStringsItr() {
+      private int idx = 0;
+      @Override
+      public boolean hasMoreElements() {
+        return busy.nextSetBit(idx) != -1;
+      }
+      @Override
+      public String nextElement() {
+        int cur = busy.nextSetBit(idx);
+        idx = cur  + 1;
+        return elt[busy.nextSetBit(cur)];
+      }
+    };
   }
 
   public void union (SetOfStrings s) {
-    // TODO 
+    SetOfStringsItr it = s.iterator();
+    while (it.hasMoreElements()) {
+      add(it.nextElement());
+    }
   }
 
   public void intersection(SetOfStrings s) {
-    // TODO 
+    SetOfStringsItr it = iterator();
+    while (it.hasMoreElements()) {
+      String e = it.nextElement();
+      if (!s.contains(e))
+        remove(e);
+    }
   }
 
   public int size() {
@@ -112,10 +171,17 @@ public class SetOfStrings {
     String b="defhijk";
     String c="hahaha";
     SetOfStrings s=new SetOfStrings();
-    s.add(a); s.add(b); s.remove(a);
-    if (s.contains(a) || s.contains(c) || !s.contains(b))
-      System.out.println("bad news...");
-    else 
+    System.out.println(s.targetIndex(a) + " : " + s);
+    s.add(a);
+    System.out.println(s.targetIndex(a) + " : " + s);
+    s.add(a);
+    System.out.println(s.targetIndex(a) + " : " + s);
+    s.add(b);
+    s.remove(a);
+    if (s.contains(a) || s.contains(c) || !s.contains(b)) {
+      System.out.println("bad news: " + s);
+    } else
       System.out.println("ok");
+
   }
 }
